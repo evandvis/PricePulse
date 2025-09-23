e61c4c24b10effe3f27ef80d50fb8e903e84aaf3
Evan Davis <evanbokinala@gmail.com>
2025-09-23 13:05:26 +0530

    created and connected to cluster

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB is connected!");
+  } catch (err) {
+    console.error("Connection failed:", err);
+  }
+};
+
+module.exports = connectDB;
