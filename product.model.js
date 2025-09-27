20d95784bebbb4b7041983a7028853290573666f
Evan Davis <evanbokinala@gmail.com>
2025-09-27 20:02:14 +0530

    added mongoose schema

---
+++ product.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  platform: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
